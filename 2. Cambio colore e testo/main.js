43525ae7845ac0e1f8bc5ab759829925728816a2
huecraft143 <mattiapalano11@gmail.com>
2024-02-26 11:14:32 +0100

    il colore del testo si alterna

--- 2. Cambio colore e testo/main.js
+++ 2. Cambio colore e testo/main.js
@@ -1,8 +1,9 @@
 
-const paragrafo = document.querySelector('.paragrafo');
+const paragrafo = document.querySelector('.classe-base');
 const bottone = document.getElementById('bottone');
 
 bottone.addEventListener('click', () => {
+  paragrafo.classList.toggle('classe-base')
   paragrafo.classList.toggle('classe-attiva');
   paragrafo.textContent = "Ho schiacciato il bottone"
 });
